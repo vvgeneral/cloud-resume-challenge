cb3d974b8d6b23456af89527685be445f490cef6
vvgeneral <asetsaylyavkhan@gmail.com>
2024-11-16 10:38:28 -0500

    Update backend.tf

--- backend/backend.tf
+++ backend/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "aset-terraform-state-bucket"
     key            = "terraform/state.tfstate"
     region         = "us-east-1"
     encrypt        = true
